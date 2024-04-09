84cdc597ff7144341824a6640e4f6ae347b90efd
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-09 14:47:44 +0100

    utils sortBynumber added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import sortByName from "./sortByName";
 import sortByPrice from "./sortByPrice";
+import sortByRating from "./sortByRating";
 
-export { sortByName, sortByPrice };
+export { sortByName, sortByPrice, sortByRating };
